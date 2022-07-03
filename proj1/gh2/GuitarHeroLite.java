aa691abdc62fdfa0211e9a2581b5ef1bd5e0e2c8
Miical <879754743@qq.com>
2022-07-03 23:38:15 +0800

    done with proj1

--- proj1/gh2/GuitarHeroLite.java
+++ proj1/gh2/GuitarHeroLite.java
@@ -9,32 +9,39 @@ public class GuitarHeroLite {
     public static final double CONCERT_A = 440.0;
     public static final double CONCERT_C = CONCERT_A * Math.pow(2, 3.0 / 12.0);
 
+    public static final String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
     public static void main(String[] args) {
-        /* create two guitar strings, for concert A and C */
-        GuitarString stringA = new GuitarString(CONCERT_A);
-        GuitarString stringC = new GuitarString(CONCERT_C);
+        GuitarString[] stringKeys = new GuitarString[keyboard.length()];
+
+        for (int i = 0; i < keyboard.length(); i += 1) {
+           stringKeys[i] = new GuitarString(440.0 * Math.pow(2.0, (i - 24.0) / 12.0));
+        }
 
         while (true) {
 
             /* check if the user has typed a key; if so, process it */
             if (StdDraw.hasNextKeyTyped()) {
                 char key = StdDraw.nextKeyTyped();
-                if (key == 'a') {
-                    stringA.pluck();
-                } else if (key == 'c') {
-                    stringC.pluck();
+                int keyIndex = keyboard.indexOf(key);
+                if (keyIndex == -1) {
+                    continue;
                 }
+                stringKeys[keyIndex].pluck();
             }
 
             /* compute the superposition of samples */
-            double sample = stringA.sample() + stringC.sample();
+            double sample = 0.0;
+            for (int i = 0; i < stringKeys.length; i += 1) {
+                sample += stringKeys[i].sample();
+            }
 
             /* play the sample on standard audio */
             StdAudio.play(sample);
 
             /* advance the simulation of each guitar string by one step */
-            stringA.tic();
-            stringC.tic();
+            for (int i = 0; i < stringKeys.length; i += 1) {
+                stringKeys[i].tic();
+            }
         }
     }
 }
